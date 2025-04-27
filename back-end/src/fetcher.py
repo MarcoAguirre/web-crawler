0b2de77ed0d70dc6b312eb7f716cc21a9ff1f592
MarcoAguirre <viniciosoria16@gmail.com>
2025-04-27 13:42:21 -0500

    refactor: Add return type for fetcher function

--- back-end/src/fetcher.py
+++ back-end/src/fetcher.py
@@ -1,12 +1,13 @@
 import requests
+from bs4 import BeautifulSoup
 
 class PageFetcher:
     def __init__(self, url):
         self.url = url
 
-    def fetch(self):
+    def fetch(self) -> BeautifulSoup:
         response = requests.get(self.url)
         if response.status_code == 200:
-            return response.content
+            return BeautifulSoup(response.content, 'html.parser')
         else:
             raise Exception(f'Failed with status code: {response.status_code}')
