e17f5ed63d15e3568cb97fccf7f69bf619571cdc
MarcoAguirre <viniciosoria16@gmail.com>
2025-04-28 01:23:53 -0500

    fix: Add CORS policy to avoid CORS error

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -1,8 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.sorted_news import router as sorted_news_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(sorted_news_router)
 
 if __name__ == "__main__":
